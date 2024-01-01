00a81093b1e9cc2577a8f9170e84529f701a1202
Kevin Hougesen <kevinhougesen@MacBook-Pro-tilhrende-Kevin-2.local>
2024-01-01 17:14:08 +0100

    fixed some bugs

--- Program.cs
+++ Program.cs
@@ -61,6 +61,7 @@ internal class Program
         app.UseStaticFiles();
 
         app.UseRouting();
+        app.UseWebSockets();
 
         app.MapBlazorHub();
         app.MapHub<ChatHub>("/chathub");

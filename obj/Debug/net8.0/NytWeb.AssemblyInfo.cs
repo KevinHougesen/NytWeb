45603649557d257787332c66c1ed388d52104032
Kevin Hougesen <kevinhougesen@MacBook-Pro-tilhrende-Kevin-2.local>
2024-01-05 16:28:09 +0100

    fixed some bugs

--- obj/Debug/net8.0/NytWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/NytWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1754b6c6d706b93efd6ca04f09e42087e49182f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

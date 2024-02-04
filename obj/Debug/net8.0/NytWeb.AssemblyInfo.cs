9756bee83def31683b64a734072bf49bac9ab517
Kevin Hougesen <kevinhougesen@MacBook-Pro-tilhrende-Kevin-2.local>
2024-02-04 16:19:40 +0100

    updating...

--- obj/Debug/net8.0/NytWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/NytWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055f9d1be753038b9e17c196685b14c4ec3fa464")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da0e3ca304f8d0ce42a784eb6061cde338df23c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NytWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
